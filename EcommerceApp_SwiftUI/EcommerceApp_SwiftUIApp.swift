0c55a509d95dc4f72c091099ceb767e759d5cd2c
pinaralper <ppinaralper@gmail.com>
2023-11-17 13:42:58 +0300

    Initial Commit

---
+++ EcommerceApp_SwiftUI/EcommerceApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceApp_SwiftUIApp.swift
+//  EcommerceApp_SwiftUI
+//
+//  Created by PINAR KALKAN on 17.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
